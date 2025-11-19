21df33feaa30dcb2c4fece8d4d0973c61917f335
Rafael López <126181633+Refex38@users.noreply.github.com>
2025-11-19 15:57:02 -0600

    Update 01_create_tables.sql

--- 01_create_tables.sql
+++ 01_create_tables.sql
@@ -1,1 +1,29 @@
+CREATE TABLE usuarios(
+  id SERIAL PRIMARY KEY,
+  nombre VARCHAR(50) NOT NULL,
+  rol INTEGER NOT NULL
+);
 
+CREATE TABLE materia_prima(
+  id SERIAL PRIMARY KEY
+);
+
+CREATE TABLE productos(
+  id SERIAL PRIMARY KEY,
+  no_parte INTEGER NOT NULL,
+  descripcion TEXT,
+  geometria GEOMETRY(CurvePolygon, 0),
+  cantidad_elementos INTEGER
+);
+
+CREATE TABLE piezas(
+
+);
+
+CREATE TABLE geometrias(
+
+);
+
+CREATE TABLE eventos(
+
+);
